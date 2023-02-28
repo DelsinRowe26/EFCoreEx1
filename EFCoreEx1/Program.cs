626266f910e8c3f56d607d0df49fb21462cbfb8d
DelsinRowe26 <ilya.gritchin20020909@gmail.com>
2023-02-28 13:10:47 +0700

    Добавьте файлы проекта.

---
+++ EFCoreEx1/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using EFCoreEx1;
+
+using (ApplicationContext db = new ApplicationContext())
+{
+	User tom = new User { Name = "Tom", Age = 33 };
+	User alice = new User { Name = "Alice", Age = 26 };
+
+	db.Users.Add(tom);
+	db.Users.Add(alice);
+	db.SaveChanges();
+	Console.WriteLine("Объекты успешно сохранены");
+
+	var users = db.Users.ToList();
+	Console.WriteLine("Спсиок объектов:");
+	foreach (User user in users)
+	{
+		Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
+	}
+}
